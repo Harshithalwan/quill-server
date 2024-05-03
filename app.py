809f5fb0197d6c56d479854363fb25f6ac3d5236
Harshit Halwan <harshithalwan@gmail.com>
2024-05-03 17:32:00 +0530

    socket io dependency

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+from flask_cors import CORS, cross_origin
 app = Flask(__name__)
+cors = CORS(app)
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@cross_origin()
+def index():
+    print("hello")
+    return "Hi there"
+
+@socketio.on('message')
+@cross_origin()
+def handle_message(message):
+    print("secondhello")
+    socketio.emit('message', "Hi there")
+
+if __name__ == '__main__':
+    socketio.run(app)
